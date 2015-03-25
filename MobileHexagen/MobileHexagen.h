190fb832a8b8a588eeb89206808b957d487b66aa
Alice Atlas <alice@atlas.ai>
2015-03-25 06:40:29 -0400

    💫 iOS now supported! ✨

---
+++ MobileHexagen/MobileHexagen.h
@@ -0,0 +1,16 @@
+  /*****\\\\
+ /       \\\\    MobileHexagen.h
+/  /\ /\  \\\\   (part of MobileHexagen)
+\  \_X_/  ////
+ \       ////    Copyright © 2015 Alice Atlas (see LICENSE.md)
+  \*****////
+
+
+#import <Foundation/Foundation.h>
+
+
+FOUNDATION_EXPORT double mobileHexagenVersionNumber;
+FOUNDATION_EXPORT const unsigned char mobileHexagenVersionString[];
+
+
+#import <MobileHexagen/Wrapper.h>
